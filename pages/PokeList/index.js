d50d87ec849f0b6713cd706426e71ca5840d112b
dagon derik farias dantas <dagonderik@gmail.com>
2022-10-14 10:24:43 -0300

    Adds API integration

--- pages/PokeList/index.js
+++ pages/PokeList/index.js
@@ -6,50 +6,56 @@ import React, { useState, useEffect } from 'react';
 import { Card, CardMedia } from '@mui/material';
 
 
+export const getStaticProps = async () => {
 
-
-
-export default function PokeList() {
+  let returnValue = null;
 
   const P = new Pokedex();
   const interval = {
-    limit: 150,
+    limit: 50,
     offset: 0
   }
 
 
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${interval.limit}&offset=${interval.offset}`);
+  const rawData = await response.json();
+  const urls = await rawData.results.map(u => u.url);
+  const pokemonRawData = await urls.map(u => fetch(u));
+  console.log(pokemonRawData);
+
+  // P.getPokemonsList(interval)
+  // .then(response => {
+  //   const urls = response.results.map(result => result.url);
+  //   Promise.all(urls.map(u => fetch(u)))
+  //   .then(response =>  Promise.all(response.map( res => res.json() )))
+  //   .then(monsters => {
+  //     returnValue = monsters;
+  //     // console.log(returnValue[0].id);
+  //   })
+  // })
+  // console.log(returnValue);
+  // return {
+  //   props: { pokemons: returnValue }
+  // }
+  return {
+    props: { pokemons: returnValue }
+  }
+}
+
+
+export default function PokeList({ pokemons }) {
+
+
   const [pokemonList, setPokemonList] = useState();
 
-  function updatePokemonList(data) {
-    setPokemonList(data);
-  }
-
-
-  const init = () => {
-    if(!pokemonList){
-      P.getPokemonsList(interval)
-      .then(response => {
-      // console.log(response);
-      const urls = response.results.map(result => result.url);
-      Promise.all(urls.map(u => fetch(u)))
-      .then(responses => Promise.all(responses.map( res => res.json())))
-      .then(monsters => {
-        updatePokemonList(monsters);
-        console.log(pokemonList);
-        });
-      });
-    }
-  }
-
-
-  
+ 
 
 	return (
         <div>
           <div className={styles.container}>
             <Header/>
             <div className={styles.main}>
-              {(pokemonList ? pokemonList.map((pokemon,i) => {
+              {(pokemons ? pokemons.map((pokemon,i) => {
                 return (
                   <Card sx={{ maxWidth:150, maxHeight:250}}>
                     <CardMedia
