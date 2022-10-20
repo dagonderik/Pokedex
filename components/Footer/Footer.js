4c1f8146cac108bd467ff0ae55862c2228ddeed3
dagon derik farias dantas <dagonderik@gmail.com>
2022-10-20 14:22:29 -0300

    Rework the pokemon card for mobile

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,14 +1,11 @@
 import style from "../../styles/Login.module.css";
 
 export default function Footer() {
-    return(
-        <div className={style.footerContainer}>
-            <a href="https://pokeapi.co/">
-                <footer className={style.footer}>
-                Powered by PokeAPI/
-                <a href="https://github.com/dagonderik">Created by @dagonderik</a>
-                </footer>
-            </a>
-        </div>
-    )
+  return (
+    <div className={style.footerContainer}>
+      <a href="https://pokeapi.co/">
+        <footer className={style.footer}>Powered by PokeAPI</footer>
+      </a>
+    </div>
+  );
 }
--- pages/PokeList/index.js
+++ pages/PokeList/index.js
@@ -1,68 +1,90 @@
-import Header from '../../components/Header/Header.js';
-import Footer from '../../components/Footer/Footer.js';
-import styles from '../../styles/PokeList.module.css';
-import { Card, CardContent, Typography } from '@mui/material';
-
-
+import Header from "../../components/Header/Header.js";
+import Footer from "../../components/Footer/Footer.js";
+import styles from "../../styles/PokeList.module.css";
+import { Card, CardContent, Typography } from "@mui/material";
 
 export const getStaticProps = async () => {
-
   const interval = {
     limit: 151,
-    offset: 0
-  }
-  
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${interval.limit}&offset=${interval.offset}`);
+    offset: 0,
+  };
+
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/?limit=${interval.limit}&offset=${interval.offset}`
+  );
   const rawData = await response.json();
-  const urls = await rawData.results.map(u => u.url);
-  const pokeList = await Promise.all(urls.map(u => fetch(u)));
-  const pokemonList = await Promise.all(pokeList.map( res => res.json()));
+  const urls = await rawData.results.map((u) => u.url);
+  const pokeList = await Promise.all(urls.map((u) => fetch(u)));
+  const pokemonList = await Promise.all(pokeList.map((res) => res.json()));
   console.log(pokemonList[0].types[0].type.name);
-  
 
   return {
-    props: { pokemons: pokemonList }
-  }
-
-}
+    props: { pokemons: pokemonList },
+  };
+};
 
 export default function PokeList({ pokemons }) {
-
-
-	return (
-        <div>
-          <div className={styles.container}>
-            <Header/>
-            <div className={styles.main}>
-              {(pokemons ? pokemons.map((pokemon,i) => {
-                return (
-                  <div className={styles.card} key={pokemon.id}>
-                    <Card className={styles.MuiCardMediaimg} sx={{ maxWidth:250, height:300}}>
-                      <div className={styles.imgcontainer}>
-                        <h3 className={styles.pokemonNumber}>{pokemon.id}</h3>
-                        <img className={styles.pokeimg} src={pokemon.sprites.front_default} height='150px'/>                
-                      </div>
-                      <CardContent style={{padding: '0'}}>
-                        <div className={styles.cardBody}>
-                          <Typography gutterBottom variant="h5" component="div" align="center">
-                            {pokemon.name}
-                          </Typography>
-                          <div variant="body2" color="text.secondary" align="center">
-                              {pokemon.types.map((type,i) => {
-                                return(
-                                  <p key={type.type.name} className={styles.types} style={{backgroundColor: `var(--${type.type.name})` }}> {type.type.name} </p>
-                                )
-                              })}
-                          </div>
+  return (
+    <div>
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.main}>
+          {pokemons ? (
+            pokemons.map((pokemon, i) => {
+              return (
+                <div className={styles.card} key={pokemon.id}>
+                  <Card
+                    className={styles.MuiCardMediaimg}
+                    sx={{ maxWidth: 250, height: 300 }}
+                  >
+                    <div className={styles.imgcontainer}>
+                      <h3 className={styles.pokemonNumber}>{pokemon.id}</h3>
+                      <img
+                        className={styles.pokeimg}
+                        src={pokemon.sprites.front_default}
+                        height="150px"
+                      />
+                    </div>
+                    <CardContent style={{ padding: "0" }}>
+                      <div className={styles.cardBody}>
+                        <Typography
+                          variant="h5"
+                          component="div"
+                          align="center"
+                        >
+                          {pokemon.name}
+                        </Typography>
+                        <div
+                          variant="body2"
+                          color="text.secondary"
+                          align="center"
+                        >
+                          {pokemon.types.map((type, i) => {
+                            return (
+                              <p
+                                key={type.type.name}
+                                className={styles.types}
+                                style={{
+                                  backgroundImage: `var(--${type.type.name})`,
+                                }}
+                              >
+                                {type.type.name}
+                              </p>
+                            );
+                          })}
                         </div>
-                      </CardContent>
-                    </Card>
-                  </div>
-                )  
-              }) : <p className={styles.loading}>Loading...</p>)}
-            </div>
-            <Footer/>
-          </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              );
+            })
+          ) : (
+            <p className={styles.loading}>Loading...</p>
+          )}
         </div>
-    )
+        <Footer />
+      </div>
+    </div>
+  );
 }
